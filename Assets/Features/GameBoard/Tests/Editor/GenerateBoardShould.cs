83c5b687cb5b4d473ab70a510006bfca6d2c8204
IgnGrillo <igngrillo@gmail.com>
2023-01-15 22:22:28 -0300

    Refactor GenerateBoardShould, using the given when then convention

--- Assets/Features/GameBoard/Tests/Editor/GenerateBoardShould.cs
+++ Assets/Features/GameBoard/Tests/Editor/GenerateBoardShould.cs
@@ -9,15 +9,21 @@ namespace Features.GameBoard.Tests.Editor
     {
         private GenerateBoard _action;
         private const int Once = 1;
-        
+
         [Test]
         public void InvokeGenerateBoardService()
         {
             var boardConfig = new BoardConfiguration(3, 3, 3);
             var boardService = Substitute.For<IBoardService>();
-            _action = new GenerateBoard(boardService);
-            _action.Invoke(boardConfig);
-            boardService.Received(Once).GenerateBoard(boardConfig);
+            GivenAnActionWith(boardService);
+            WhenInvoke(boardConfig);
+            ThenGenerateBoardWithConfig(boardService, boardConfig);
         }
+
+        private void GivenAnActionWith(IBoardService boardService) => _action = new GenerateBoard(boardService);
+        private void WhenInvoke(BoardConfiguration boardConfig) => _action.Invoke(boardConfig);
+
+        private static void ThenGenerateBoardWithConfig(IBoardService boardService, BoardConfiguration boardConfig) =>
+                boardService.Received(Once).GenerateBoard(boardConfig);
     }
 }
